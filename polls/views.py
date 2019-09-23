150f4ff1b513129f91b0644614901e6d875bd55c
CiceroRegis <ciceroregis25@gmail.com>
2019-09-23 14:18:08 -0300

    tutorial04 form included

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-
-from polls.models import Question
+from django.urls import reverse
+from polls.models import Question, Choice
 
 
 def index(request):
@@ -22,4 +22,20 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("you' re voting on questions %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn' select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponse(reverse('polls:results', args=question.id), )
+
